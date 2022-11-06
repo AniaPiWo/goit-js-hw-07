e0af68db9c13a1785c1cc47b63a05aecb073491b
AniaPiWo <snowflake@wp.pl>
2022-11-06 16:08:10 +0100

    up

---
+++ js/02-lightbox.js
@@ -0,0 +1,15 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+for (var image of galleryItems) {
+  
+  gallery.insertAdjacentHTML("beforeend", `
+   <a class="gallery__item" href="${image.original}">
+  <img class="gallery__image"src="${image.preview}" alt="${image.desciption}" />
+</a>
+  `);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {});
