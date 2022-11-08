929519d689932b443d0c9d19ed621259fa896e09
AniaPiWo <snowflake@wp.pl>
2022-11-08 12:07:45 +0100

    pop2 comms

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,19 +15,24 @@ for (var image of galleryItems) {
   `);
 }
 
+
+//modal
 gallery.addEventListener('click', (event) => {
   
   event.preventDefault();
 
+  //avoid the image previewing when we click between the images
   if (event.target.nodeName !== "IMG") {
     return;
   }
 
+  //light box create and show
   const instance = basicLightbox.create(
     `<img src="${event.target.dataset.source}" alt ="${event.target.alt}">`)
   console.log(event.target.dataset.source);
   instance.show();
 
+  // on esc close
   document.addEventListener("keydown", (event) => {
     const visible = basicLightbox.visible();
     if (visible && event.key === "Escape") {
